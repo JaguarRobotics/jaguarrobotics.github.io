2afd0f392a9405dea1258619b57e3c10d9d15b2f
Zach Deibert <zachdeibert@gmail.com>
2017-08-10 13:48:26 -0500

    Added parallax header

--- src/components/HeaderImage.js
+++ src/components/HeaderImage.js
@@ -1,11 +1,18 @@
 import React from "react";
+import {Parallax, ParallaxImage} from "./Parallax";
+import person from "./person.png";
+import robot from "./robot.png";
 import "./HeaderImage.css";
 
 export default class HeaderImage extends React.Component {
     render() {
         return (
             <div className="header-image">
-
+                <Parallax>
+                    <ParallaxImage src={person} x={30} y={50} z={50} width={100} height={150} />
+                    <ParallaxImage src={person} x={70} y={50} z={50} width={100} height={150} />
+                    <ParallaxImage src={robot} x={50} y={20} z={100} width={150} height={150} />
+                </Parallax>
             </div>
         );
     }
---
+++ src/components/Parallax.js
@@ -0,0 +1,62 @@
+import React from "react";
+import "./Parallax.css";
+
+export class ParallaxImage extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <img className="parallax-image" src={this.props.src} alt="" style={{
+                "bottom": (this.props.y + this.props.z * this.props.getState().y) + "%",
+                "left": (this.props.x + this.props.z * this.props.getState().x) + "%",
+                "width": this.props.width + "px",
+                "height": this.props.height + "px",
+                "marginLeft": "-" + (this.props.width / 2) + "px",
+                "marginBottom": "-" + (this.props.height / 2) + "px"
+            }} />
+        );
+    }
+}
+
+export class Parallax extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            "x": 0,
+            "y": 0
+        };
+        this.getState = this.getState.bind(this);
+        this.handleMouseMove = this.handleMouseMove.bind(this);
+    }
+
+    componentDidMount() {
+        document.body.addEventListener("mousemove", this.handleMouseMove);
+    }
+
+    componentWillUnmount() {
+        document.body.removeEventListener("mousemove", this.handleMouseMove);
+    }
+
+    handleMouseMove(ev) {
+        this.setState({
+            "x": 0.5 - ev.clientX / document.documentElement.clientWidth,
+            "y": ev.clientY / document.documentElement.clientHeight - 0.5
+        });
+    }
+
+    getState() {
+        return this.state;
+    }
+
+    render() {
+        return (
+            <div className="parallax">
+                {React.Children.map(this.props.children, child => React.cloneElement(child, {
+                    "getState": this.getState
+                }))}
+            </div>
+        );
+    }
+}
